7cc0897a3bf9c56c8c2e973a3eaf761238fbd66a
starchyunderscore <starchy_underscore@gmx.com>
2024-01-16 09:59:00 -0500

    working on it

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import curses
 import os
 import sys
+from PIL import Image
 
 # Try to get image path as given as command argument
 try:
     img_path = str(sys.argv[1])
-except:
+    im = Image.open(img_path) # Original image, remains unchanged
+    imc = Image.open(img_path) # Copy of image, where changes are applied
+except Exception as ex:
+    print(ex)
     pass
 
 '''
 - f1: invert
 - f2: grayscale
-- f3: rotate anticlockwise
-- f4: rotate clockwise
+- f3: rotate (cursor keys)
+- f4: flip (cursor keys)
 
 - f5: crop
-- f6: hue adjust
-- f10: save (overwrite)
-- f8: save (copy)
+- f6: advanced (hue, saturation, brightness)
+- f10: 
+- f8: save (overwrite/copy)
 
-- f9: open new image (save/discard)
+- f9: open new image (save(overwrite/copy)/discard)
 - f10: fullscreen the original image
 - f11: fullscreen the edit preview
 - f12: quit (save/discard)
 '''
+
+placeholder_no_image_selected = "No image selected"
 
 topbar_fkeys = "f1\tf2\tf3\tf4\tf5\tf6\tf7\tf8\tf9\tf10\tf11\tf12\n"
 topbar_uses  = "f1\tf2\tf3\tf4\tf5\tf6\tf7\tf8\tf9\tf10\tf11\texit\n"
@@ -34,12 +40,20 @@ def main(win):
     win.clear()
     win.addstr(topbar_fkeys)
     win.addstr(topbar_uses)
+    try:
+        win.addstr(img_path)
+    except:
+        win.addstr(placeholder_no_image_selected)
     while 1:
         try:
             key = win.getkey()
             win.clear()
             win.addstr(topbar_fkeys)
             win.addstr(topbar_uses)
+            try:
+                win.addstr(img_path)
+            except:
+                win.addstr(placeholder_no_image_selected)
             match key:
                 case "KEY_F(1)": # 
                     pass
@@ -57,8 +71,16 @@ def main(win):
                     pass
                 case "KEY_F(8)": # 
                     pass
-                case "KEY_F(9)": # 
-                    pass
+                case "KEY_F(9)": # Open new image
+                    win.clear()
+                    win.addstr("Input Filename: ")
+                    img_path = strip(win.getstr())
+                    im = Image.open(img_path)
+                    imc = Image.open(img_path)
+                    if img_path == "":
+                        pass
+                    else:
+                        pass # Will be replaced with asking the user if they wish to discard the old image
                 case "KEY_F(10)": # 
                     pass
                 case "KEY_F(11)": # 
@@ -68,5 +90,6 @@ def main(win):
         except Exception as e:
             # No input
             pass
-
+            
+print(img_path)
 curses.wrapper(main)
